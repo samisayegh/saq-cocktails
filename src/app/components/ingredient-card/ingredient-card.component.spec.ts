8c31f7bcb0e4fc45dd1d571a628d5a3e2af81a03
Sami Sayegh <samisayegh1@gmail.com>
2017-05-28 20:20:18 -0400

    renaming ingredient component to ingredient-card

--- src/app/components/ingredient-card/ingredient-card.component.spec.ts
+++ src/app/components/ingredient-card/ingredient-card.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { IngredientComponent } from './ingredient.component';
+import { IngredientCardComponent } from './ingredient-card.component';
 
-describe('IngredientComponent', () => {
-  let component: IngredientComponent;
-  let fixture: ComponentFixture<IngredientComponent>;
+describe('IngredientCardComponent', () => {
+  let component: IngredientCardComponent;
+  let fixture: ComponentFixture<IngredientCardComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ IngredientComponent ]
+      declarations: [ IngredientCardComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(IngredientComponent);
+    fixture = TestBed.createComponent(IngredientCardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ src/app/components/ingredient-card/ingredient-card.component.ts
@@ -0,0 +1,90 @@
+import { Component, Input, Output, EventEmitter, OnInit, OnDestroy} from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+import { SaqService } from '../../services/saq/saq.service';
+import { Maths } from '../../../utils/maths';
+
+const INGREDIENT_PHOTOS_PATH = '../../../assets/ingredient-photos/';
+
+@Component({
+    selector: 'app-ingredient-card',
+    templateUrl: './ingredient-card.component.html',
+    styleUrls: ['./ingredient-card.component.css'],
+    providers: [SaqService]
+})
+
+export class IngredientCardComponent implements OnInit, OnDestroy {
+    @Input() isAlcohol: boolean;
+    @Input() name: string;
+    @Input() quantity: number | string;
+    // emits selected alcohol for aggregation in parent recipe component
+    @Output() alcoholSelected: EventEmitter<Type.SelectedAlcohol> = new EventEmitter();
+
+    subscription: Subscription = new Subscription();
+    selectedResultIndex: number = 0;
+    selectedResult: Type.Result = null;
+
+    constructor(private saqService: SaqService) {}
+
+    ngOnInit() {
+        this.subscription = this.saqService.selectedResult
+        .subscribe(result => {
+            this.selectedResult = result;
+            this.alcoholSelected.emit({name: this.name, selected: result})
+        });
+
+        this.saqService.listProducts(this.name);
+    }
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
+    }
+
+    formattedIngredientTitle(): string {
+        let formattedNum = this.quantity;
+
+        if (typeof formattedNum === 'number') {
+            formattedNum = Maths.convertDecimalToFraction(formattedNum) + ' oz. of';
+        }
+
+        return `${formattedNum} ${this.name}`;
+    }
+    
+    getImageUrl(): string {
+        let imageUrl = null;
+
+        if (this.isAlcohol && this.selectedResult) {
+            imageUrl = this.selectedResult.raw.tpthumbnailuri;
+        }
+
+        if (!this.isAlcohol) {
+            const fileName = this.name.toLowerCase().replace(' ', '_') + '.jpg';
+            imageUrl = (INGREDIENT_PHOTOS_PATH + fileName);
+        }
+
+        return imageUrl;
+        // return (imageUrl) ? imageUrl : INGREDIENT_PHOTOS_PATH + 'mint_leaves.jpg';        
+    }
+
+
+    updateSelectedResult(num: number) {
+        const results = this.saqService.results;
+        if (!results) {return}
+
+        const maxIndex = results.length - 1;
+        const newIndex = this.selectedResultIndex + num;
+
+        if (newIndex > maxIndex) {
+            this.selectedResultIndex = 0;
+        }
+        else if (newIndex < 0) {
+            this.selectedResultIndex = maxIndex;
+        }
+        else {
+            this.selectedResultIndex = newIndex;
+        }
+
+        this.saqService.updateSelectedResult(this.selectedResultIndex);
+    }
+}
